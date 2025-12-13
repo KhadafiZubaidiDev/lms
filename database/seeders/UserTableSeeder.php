ff3d3eff64048e521a3a52b4855975039ebff398
Khadafi Zubaidi <128591613+KhadafiZubaidiDev@users.noreply.github.com>
2025-12-14 06:07:06 +0800

    Seed Demo User Data 3

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -24,6 +24,26 @@ class UserTableSeeder extends Seeder
                 'username' => 'admin',
                 'email' => 'admin@gmail.com',
                 'password' => Hash::make('password'),
+                'role' => 'admin',
+                'status' => 1
+            ],
+            //Instructor
+            [
+                'name' => 'Instructor',
+                'username' => 'instructor',
+                'email' => 'instructor@gmail.com',
+                'password' => Hash::make('password'),
+                'role' => 'instructor',
+                'status' => 1
+            ],
+            //User
+            [
+                'name' => 'User',
+                'username' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('password'),
+                'role' => 'user',
+                'status' => 1
             ]
         ]);
     }
