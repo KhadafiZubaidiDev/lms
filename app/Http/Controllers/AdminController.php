b5036779608c8372034eaba00f3855a94a5a0d26
Khadafi Zubaidi <128591613+KhadafiZubaidiDev@users.noreply.github.com>
2025-12-14 06:42:18 +0800

    Multi Auth 6

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,4 +7,9 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     //
+    public function AdminDashboard(){
+
+        return view('admin.admin_dashboard');
+
+    } // End Method
 }
