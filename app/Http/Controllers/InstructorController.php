de42bc4820267e0d131bc32b33adee14a5544176
Khadafi Zubaidi <128591613+KhadafiZubaidiDev@users.noreply.github.com>
2025-12-14 06:48:59 +0800

    Multi Auth 7

--- app/Http/Controllers/InstructorController.php
+++ app/Http/Controllers/InstructorController.php
@@ -7,4 +7,9 @@ use Illuminate\Http\Request;
 class InstructorController extends Controller
 {
     //
+    public function InstructorDashboard(){
+
+        return view('instructor.instructor_dashboard');
+
+    } // End Method
 }
