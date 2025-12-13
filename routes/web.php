eff4d94cd1e19d77faf1e04ea877666e02adc6c0
Khadafi Zubaidi <128591613+KhadafiZubaidiDev@users.noreply.github.com>
2025-12-14 06:31:56 +0800

    Multi Auth 5

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\InstructorController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
